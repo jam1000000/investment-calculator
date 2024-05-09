23da077250bc20dbedec27ceeb94ab43b7b5595b
jam1000000 <87884815+jam1000000@users.noreply.github.com>
2024-05-09 01:21:45 -0500

    Update investcalc.cpp
    New variables for total at end of investment periods (totalinv) and a yield calculation (yield). Takes user input for each user-dependent value. Prints total investment amount as well as yield.

--- investcalc.cpp
+++ investcalc.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    float init = 285;
-    float reinv = 91;
-    int per = 25;
-    float interest = 1.029;
+    float init;
+    float reinv;
+    int per;
+    float interest;
+    
+    
+    cout << "Enter an initial investment: ";
+    cin >> init;
+    
+    cout << "Reinvestment amount: ";
+    cin >> reinv;
+    
+    cout << "Enter the number of periods: ";
+    cin >> per;
+    
+    cout << "Percent interest: ";
+    cin >> interest;
+    interest /= 100;
+    interest += 1.0;
+    
+    float totalinv = init;
 
     for (int i = 0; i < per; i++) {
-        init *= interest;
-        init += reinv;
+        totalinv *= interest;
+        totalinv += reinv;
     }
-
-    std::cout << init;
+    
+    float yield = totalinv - (init + (reinv * per));
+    
+    cout << "Your investment amount at the end of " << per << " periods is now $" << totalinv << ".\n";
+    cout << "With an initial investment of $" << init << ", You made $" << yield << ".\n";
 
     return 0;
 }
